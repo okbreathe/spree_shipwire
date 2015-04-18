01bb82167b99ad28607ef81825a0e9546100a310
Josh Nussbaum <joshnuss@gmail.com>
2015-04-18 14:41:54 -0400

    Add source_route for install generator

--- lib/generators/spree_shipwire/install/install_generator.rb
+++ lib/generators/spree_shipwire/install/install_generator.rb
@@ -1,6 +1,7 @@
 module SpreeShipwire
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
 
       class_option :auto_run_migrations, type: :boolean, default: false
 
