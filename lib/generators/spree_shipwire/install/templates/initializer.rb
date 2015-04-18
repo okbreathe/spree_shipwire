468387390878938e18d436e7096c63a78d85a5a9
Josh Nussbaum <joshnuss@gmail.com>
2015-04-18 14:49:14 -0400

    Add notes to initializer template

--- lib/generators/spree_shipwire/install/templates/initializer.rb
+++ lib/generators/spree_shipwire/install/templates/initializer.rb
@@ -1,6 +1,15 @@
 SpreeShipwire.config do |config|
+  # account email address
   config.username = ENV['SHIPWIRE_USERNAME']
+
+  # account password
   config.password = ENV['SHIPWIRE_PASSWORD']
+
+  # For testing use: Test
+  # For production use: Production
   config.server   = ENV['SHIPWIRE_SERVER']
+
+  # For testing use: https://api.beta.shipwire.com/exec/
+  # For production use: https://api.shipwire.com/exec/
   config.endpoint = ENV['SHIPWIRE_ENDPOINT']
 end
