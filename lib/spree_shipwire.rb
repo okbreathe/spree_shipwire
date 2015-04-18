cff8dc308c8da4377064705a362fa337d8e2a9f2
Josh Nussbaum <joshnuss@gmail.com>
2015-04-18 15:59:52 -0400

    Added autoloading of SpreeShipwire::Rates

--- lib/spree_shipwire.rb
+++ lib/spree_shipwire.rb
@@ -3,6 +3,8 @@ require 'shipwire'
 require 'spree_shipwire/engine'
 
 module SpreeShipwire
+  autoload :Rates, 'spree_shipwire/rates'
+
   def self.config(&block)
     Shipwire.configure(&block)
   end
