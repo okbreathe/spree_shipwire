506d050fad422e82dce01eaa673e913ff5b83245
Josh Nussbaum <joshnuss@gmail.com>
2015-04-18 20:35:17 -0400

    Added UnrecognizedSKUError

--- lib/spree_shipwire.rb
+++ lib/spree_shipwire.rb
@@ -3,10 +3,11 @@ require 'shipwire'
 require 'spree_shipwire/engine'
 
 module SpreeShipwire
-  autoload :Rates,           'spree_shipwire/rates'
-  autoload :ConnectionError, 'spree_shipwire/errors'
-  autoload :RateError,       'spree_shipwire/errors'
-  autoload :AddressError,    'spree_shipwire/errors'
+  autoload :Rates,                'spree_shipwire/rates'
+  autoload :ConnectionError,      'spree_shipwire/errors'
+  autoload :RateError,            'spree_shipwire/errors'
+  autoload :AddressError,         'spree_shipwire/errors'
+  autoload :UnrecognizedSKUError, 'spree_shipwire/errors'
 
   def self.config(&block)
     Shipwire.configure(&block)
--- lib/spree_shipwire/errors.rb
+++ lib/spree_shipwire/errors.rb
@@ -2,4 +2,5 @@ module SpreeShipwire
   class ConnectionError < StandardError; end
   class RateError < StandardError; end
   class AddressError < StandardError; end
+  class UnrecognizedSKUError < StandardError; end
 end
